cb04ff69a83c15960dc0629eb33507d11114936c
Adil Abed <dr.sivana@protonmail.com>
2017-11-17 17:15:41 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(string = "Squawk!")
-  string = gets.strip
-  puts "#{string}"
-  return string
+def parrot(phrase = "Squawk!")
+  phrase = gets.strip
+  puts "#{phrase}"
+  return phrase
 
 end
